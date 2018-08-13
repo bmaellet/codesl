bc9f52754f9c42a3ff3e002932bebf9552c9f99b
bmaellet <mn_mayer20@outlook.com>
2018-08-13 18:08:35 +0000

    username and password added

---
+++ python-trainings/user_pass.py
@@ -0,0 +1,7 @@
+userName = input("Enter your name please: ")
+userAge = input("What is your age: ")
+
+print()
+print("Welcome {}.".format(userName))
+print(userName + " you age is {}.".format(userAge))
+print("Thank you!!")
